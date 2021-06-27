51ae85e559b94a2e47872cfd0ab32662d7ce9ac6
Sukanya68 <86548403+Sukanya68@users.noreply.github.com>
2021-06-27 12:48:52 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js</h1>
+      <p>
+        A basic web dev React js Bootcamp conducted by ShapeAI 7 days long free
+        Bootcamp
+      </p>
+    </div>
+  );
+}
+export default Info;
